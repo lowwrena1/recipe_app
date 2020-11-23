277860fa3d838a7909a8512bf1625c07f1ab5414
Lorena Ontiveros <lorena0328o@gmail.com>
2020-11-23 16:28:50 -0600

    api info

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>my react app</h1>
-     <h2>lets see if this works</h2>
+const App = () => {
+
+  const APP_ID = '707c9da5';
+  const APP_KEY = 'ed1e5ec58f72e45846092cf555723422';
+
+  const expampleReq =
+  "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}"
+  
+  return(
+    <div className = "App">
+      <h1>Hello React</h1>
     </div>
   );
-}
+};
 
 export default App;
