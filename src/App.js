b84053c340ceb2ec944bc5cb8d2d9b523f8bef58
Lorena Ontiveros <lorena0328o@gmail.com>
2020-12-14 13:52:50 -0600

    created search bar inputarea

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ const App = () => {
   const APP_KEY = 'ed1e5ec58f72e45846092cf555723422';
 
   const expampleReq =
-  "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}"
+  'https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}';
   
   return(
     <div className = "App">
-      <h1>Hello React</h1>
+      <h1>Recipe Finder</h1>
+      <form classname="search-form"></form>
+      <input classname="search-bar" type="text"/>
+      <button classname="search-button" type='submit'>search</button>
     </div>
   );
 };
